b782d26063c378f7ff915c54a9fe398978c4c2a5
tico88612 <17496418+tico88612@users.noreply.github.com>
2023-04-13 11:54:43 +0800

    change(migration): add category unknown
    - cite & author can set null
    - category_id default 0

--- db/migrations/000001_create_sentences_table.up.sql
+++ db/migrations/000001_create_sentences_table.up.sql
@@ -2,12 +2,16 @@ CREATE TABLE IF NOT EXISTS categories(
    category_id serial PRIMARY KEY,
    category_name VARCHAR (100) NOT NULL
 );
+
+INSERT INTO "categories" ("category_id", "category_name")
+VALUES ('0', '未分類');
+
 CREATE TABLE IF NOT EXISTS sentences(
    id serial PRIMARY KEY,
    sentence VARCHAR (200) NOT NULL,
-   category_id INT,
-   cite VARCHAR (50) NOT NULL,
-   author VARCHAR (50) NOT NULL,
+   category_id INT DEFAULT 0,
+   cite VARCHAR (50),
+   author VARCHAR (50),
    created_at TIMESTAMP DEFAULT NOW(),
    CONSTRAINT fk_category
       FOREIGN KEY(category_id) 
