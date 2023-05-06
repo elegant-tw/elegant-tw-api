80f7bc55a68dca5f3385d7b834a315f6b0f8a63e
tico88612 <17496418+tico88612@users.noreply.github.com>
2023-05-06 11:51:30 +0800

    feat: sentence repo add with toxic
    - GetRandomSentence refactor

--- sentence/repository/postgresql/postgresql.go
+++ sentence/repository/postgresql/postgresql.go
@@ -17,7 +17,28 @@ func NewpostgresqlSentenceRepoistory(db *sql.DB) domain.SentenceRepository {
 }
 
 func (p *postgresqlSentenceRepository) GetRandomSentence(ctx context.Context) (*domain.Sentence, error) {
-	row := p.db.QueryRow("SELECT id, sentence, category_id, cite, author FROM sentences ORDER BY RANDOM() LIMIT 1")
+	row := p.db.QueryRow(
+		`SELECT sentences.id, sentences.sentence, sentences.category_id, sentences.cite, sentences.author
+		FROM sentences, categories
+		WHERE categories.is_toxic = False AND sentences.category_id = categories.category_id
+		ORDER BY RANDOM()
+		LIMIT 1;`,
+	)
+	d := &domain.Sentence{}
+	if err := row.Scan(&d.ID, &d.Sentence, &d.Category, &d.Cite, &d.Author); err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	return d, nil
+}
+
+func (p *postgresqlSentenceRepository) GetRandomSentenceWithToxic(ctx context.Context) (*domain.Sentence, error) {
+	row := p.db.QueryRow(
+		`SELECT id, sentence, category_id, cite, author
+		FROM sentences
+		ORDER BY RANDOM()
+		LIMIT 1;`,
+	)
 	d := &domain.Sentence{}
 	if err := row.Scan(&d.ID, &d.Sentence, &d.Category, &d.Cite, &d.Author); err != nil {
 		logrus.Error(err)
